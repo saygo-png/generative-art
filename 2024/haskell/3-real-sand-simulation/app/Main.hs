fb7c17207a798217cf6bb860029d0c546986f4a7
saygo-png <saygo.mail@proton.me>
2024-07-17 06:06:06 +0200

    trying badly to implement falling

--- 2024/haskell/3-real-sand-simulation/app/Main.hs
+++ 2024/haskell/3-real-sand-simulation/app/Main.hs
@@ -1,29 +1,58 @@
+-- Imports. {{{
 import Data.Set qualified as S -- [S]et
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
-import RIO hiding (Display, Down)
 
---------------
--- Settings --
---------------
+-- }}}
 
+-- Settings.  {{{
 width, height, offset, fps :: Int
 width = 500
 height = width
 offset = 0
 fps = 144
 
+initialState :: State
+initialState =
+  MkState
+    { cellCount = 3,
+      gap = 10,
+      cellSize = widthF / cellCount initialState,
+      cellMatrix = createCellWire initialState,
+      keys = S.empty
+    }
+
+{- ORMOLU_DISABLE -}
+-- Functions which take an alpha 0-255 value and return a transparent color. [g]ruvbox.
+gDark1, gDark2, gDark3, gDark4, gLight5, gLight6, gLight7, gLight8, gRed, gOrange, gYellow, gGreen, gCyan, gBlue, gPurple, gBrown :: Int -> Color
+gDark1  = makeColorI 40  40  40  -- #282828
+gDark2  = makeColorI 60  56  54  -- #3c3836
+gDark3  = makeColorI 80  73  69  -- #504945
+gDark4  = makeColorI 102 92  84  -- #665c54
+gLight5 = makeColorI 189 174 147 -- #bdae93
+gLight6 = makeColorI 213 196 161 -- #d5c4a1
+gLight7 = makeColorI 235 219 178 -- #ebdbb2
+gLight8 = makeColorI 251 241 199 -- #fbf1c7
+gRed    = makeColorI 251 73  52  -- #fb4934
+gOrange = makeColorI 254 128 25  -- #fe8019
+gYellow = makeColorI 250 189 47  -- #fabd2f
+gGreen  = makeColorI 184 187 38  -- #b8bb26
+gCyan   = makeColorI 142 192 124 -- #8ec07c
+gBlue   = makeColorI 131 165 152 -- #83a598
+gPurple = makeColorI 211 134 155 -- #d3869b
+gBrown  = makeColorI 214 93  14  -- #d65d0e
+{- ORMOLU_ENABLE -}
+
 widthF, heightF :: Float
 widthF = fromIntegral width
 heightF = fromIntegral height
 
 background :: Color
-background = makeColorI 40 40 40 255
+background = gDark1 255
 
------------------
--- Boilerplate --
------------------
+-- }}}
 
+-- Boilerplate. {{{
 main :: IO ()
 main =
   play
@@ -38,77 +67,125 @@ main =
 window :: Display
 window = InWindow "Sand" (width, height) (offset, offset)
 
---
+-- }}}
+
+-- Main loop. {{{
 
 -- | Update the game state.
-update :: Float -> SandSimulation -> SandSimulation
-update _ game = game
+update :: Float -> State -> State
+update _ = turnCellOn 3 2
 
-render :: SandSimulation -> Picture
-render _ = let
-    centerAmount = (-((widthF / 2)))
-  in
-  pictures
-    [ color (greyN 0.3) (rectangleSolid widthF heightF),
-      translate centerAmount centerAmount $ pictures (fillGrid (makeColorI 250 189 47 255) (createGridWire (getCellCount initialGrid)))
-    ]
+-- | Render the game state.
+render :: State -> Picture
+render state =
+  let cellSize' = cellSize state
+      gap' = gap state
+      cellCount' = cellCount state
+      centerAmount = (-((widthF / 2) - cellSize' / 2 + ((gap' * cellCount') / 2)))
+   in pictures
+        [ color (gDark2 127) (rectangleSolid widthF heightF),
+          translate centerAmount centerAmount $ pictures (fillCells state)
+        ]
 
-data Grid = MkGrid
+-- }}}
+
+-- Data Structures. {{{
+data State = MkState
   { -- | Amount of cells
-    getCellCount :: Float,
-    getCellSize :: Float,
-    getGap :: Float
-  }
-
-initialGrid :: Grid
-initialGrid =
-  MkGrid
-    { getCellCount = 10,
-      getCellSize = widthF / getCellCount initialGrid,
-      getGap = 20
-    }
-
-data SandSimulation = State
-  { -- | Total sand particle amount.
-    sandAmount :: Int,
+    cellCount :: Float,
+    -- | Size of one cell
+    cellSize :: Float,
+    -- | Gap between the cells, makes the grid larger.
+    gap :: Float,
+    -- | multidimensional array of points, x = column, y = row.
+    cellMatrix :: [[Cell]],
     -- | Which keys are pressed
     keys :: S.Set Key
   }
-  deriving (Show)
 
-initialState :: SandSimulation
-initialState =
-  State
-    { sandAmount = 0,
-      keys = S.empty
-    }
+-- | Type defining a cell on a grid, a cell can be on or off, true = on, false = off.
+type Cell = (Point, Bool)
 
-createGridWire :: Float -> [[Point]]
-createGridWire cellCount = horizontalLineCells $ verticalLineCells cellCount 0
+-- }}}
+
+-- Grid setup. {{{
+
+-- | Creates a multidimensional array of points, x = column, y = row
+createCellWire :: State -> [[Cell]]
+createCellWire grid = gridWireIntoCells . horizontalLineCells $ verticalLineCells cellCount' 0
   where
-    cellSize = fromIntegral width / cellCount
-    gap = getGap initialGrid
+    cellCount' = cellCount grid
+    cellSize' = cellSize grid
+    gap' = gap grid
     verticalLineCells :: Float -> Float -> [Point]
     verticalLineCells 0 _ = []
-    verticalLineCells n acc = (0, acc) : verticalLineCells (n - 1) (acc + cellSize + gap)
+    verticalLineCells n acc = (0, acc) : verticalLineCells (n - 1) (acc + cellSize' + gap')
 
     horizontalLineCells :: [Point] -> [[Point]]
-    horizontalLineCells = map (toRow cellCount 0)
+    horizontalLineCells = map (toRow cellCount' 0)
       where
         toRow :: Float -> Float -> Point -> [Point]
         toRow 0 _ _ = []
-        toRow n acc (vx, vy) = (vx + acc, vy) : toRow (n - 1) (acc + cellSize + gap) (vx, vy)
+        toRow n acc (vx, vy) = (vx + acc, vy) : toRow (n - 1) (acc + cellSize' + gap') (vx, vy)
 
-fillGrid :: Color -> [[Point]] -> [Picture]
-fillGrid color' = concatMap (map rectOnPoint)
+    -- \| Converts a Point grid to a Cell grid.
+    gridWireIntoCells :: [[Point]] -> [[Cell]]
+    gridWireIntoCells = map (map pointToCell)
+      where
+        -- \| Converts a Point to a Cell.
+        pointToCell :: Point -> Cell
+        pointToCell (x, y) = ((x, y), False)
+
+-- | Draw a cell on each point.
+fillCells :: State -> [Picture]
+fillCells grid = concatMap (map rectOnCell) (cellMatrix grid)
   where
-    cellCount = getCellCount initialGrid
-    cellSize = fromIntegral width / cellCount
-    rectOnPoint :: Point -> Picture
-    rectOnPoint (x, y) = translate x y $ color color' (rectangleSolid cellSize cellSize)
+    cellSize' = cellSize grid
+    rectOnCell :: Cell -> Picture
+    rectOnCell ((x, y), True) = translate x y $ color (gGreen 255) (rectangleSolid cellSize' cellSize')
+    rectOnCell ((x, y), False) = translate x y $ color (gDark3 255) (rectangleSolid cellSize' cellSize')
+
+-- }}}
+
+-- Cell Logic. {{{
+
+-- | Make the cell fall
+fall :: Cell -> State
+fall = undefined
+
+-- | Get a cell based on row, column. 1 indexed.
+getCell :: Int -> Int -> State -> Cell
+getCell rowIndex colIndex state =
+  let array = cellMatrix state
+      rowIndex' = rowIndex - 1
+      colIndex' = colIndex - 1
+   in ((array !! rowIndex') !! colIndex')
+
+filterTrue :: [[Cell]] -> [Cell]
+filterTrue nestedList = filter (\((_, _), b) -> b) (concat nestedList)
+
+-- | Update an element at a specific position, 1 indexed
+turnCellOn :: Int -> Int -> State -> State
+turnCellOn rowIndex' colIndex' grid = grid {cellMatrix = cellMatrix'}
+  where
+    array = cellMatrix grid
+    rowIndex = rowIndex' - 1
+    colIndex = colIndex' - 1
+    -- Extract the row
+    row = array !! rowIndex
+    -- Update the specific element in the row
+    updatedRow = take colIndex row ++ [updatedElement] ++ drop (colIndex + 1) row
+      where
+        updatedElement = let ((x, y), _) = row !! colIndex in ((x, y), True)
+    -- Reconstruct the list with the updated row
+    cellMatrix' = take rowIndex array ++ [updatedRow] ++ drop (rowIndex + 1) array
+
+-- }}}
+
+-- Input Handling. {{{
 
 -- | Respond to key events.
-handleInput :: Event -> SandSimulation -> SandSimulation
+handleInput :: Event -> State -> State
 
 -- | Register j as a key press.
 handleInput (EventKey (Char 'j') Down _ _) game =
@@ -117,3 +194,7 @@ handleInput (EventKey (Char 'j') Up _ _) game =
   game {keys = S.delete (Char 'j') (keys game)}
 -- Do nothing for all other events.
 handleInput _ game = game
+
+-- }}}
+
+-- vim:foldmethod=marker
