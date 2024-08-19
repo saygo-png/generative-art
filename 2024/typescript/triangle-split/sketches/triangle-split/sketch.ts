5a6d61869fc2ad5e83f719bd812b10d4d06e62c0
saygo-png <saygo.mail@proton.me>
2024-08-19 20:32:39 +0200

    fixes

--- 2024/typescript/triangle-split/sketches/triangle-split/sketch.ts
+++ 2024/typescript/triangle-split/sketches/triangle-split/sketch.ts
@@ -1,10 +1,8 @@
-// GLOBAL VARS & TYPES
-let recursions_slider: p5.Element;
-
+// GLOBAL VARS & TYPES {{{
 class Point {
   constructor(
     public x: number,
-    public y: number,
+    public y: number
   ) {}
   get(): [number, number] {
     return [this.x, this.y];
@@ -14,16 +12,32 @@ class Triangle {
   constructor(
     public p1: Point,
     public p2: Point,
-    public p3: Point,
+    public p3: Point
   ) {}
   get(): [number, number, number, number, number, number] {
     return [this.p1.x, this.p1.y, this.p2.x, this.p2.y, this.p3.x, this.p3.y];
   }
 }
 
-const unp = Object.values;
+const seedTriangle = new Triangle(
+  new Point(-160, 100),
+  new Point(160, 100),
+  new Point(0, -170)
+);
 
-// SETTINGS
+const edges = [
+  [seedTriangle.p1, seedTriangle.p3],
+  [seedTriangle.p3, seedTriangle.p2],
+  [seedTriangle.p2, seedTriangle.p1],
+];
+
+const widthBy2 = width / 2;
+const heightBy2 = height / 2;
+
+const recursions_slider: p5.Element = createSlider(1, 15, 5, 1)
+  .position(10, 10)
+  .style('background', 'green');
+// }}}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function setup() {
   createCanvas(1000, 1000);
@@ -31,15 +45,7 @@ function setup() {
   stroke(40);
   strokeWeight(0);
   rectMode(CENTER);
-  recursions_slider = createSlider(1, 15, 5, 1)
-    .position(10, 10)
-    .style("background", "green");
 }
-const seedTriangle = new Triangle(
-  new Point(-160, 100),
-  new Point(160, 100),
-  new Point(0, -170),
-);
 
 const midpoint = (p1: Point, p2: Point): Point =>
   new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
@@ -56,7 +62,7 @@ function endpoint(p1: Point, p2: Point, length: number): Point {
   const scaledUnitVec = new Point(unitVec.x * length, unitVec.y * length);
   return new Point(
     midpoint_.x + scaledUnitVec.x,
-    midpoint_.y + scaledUnitVec.y,
+    midpoint_.y + scaledUnitVec.y
   );
 }
 
@@ -64,7 +70,7 @@ function splitRecursively(
   p1: Point,
   p2: Point,
   length: number,
-  depth: number,
+  depth: number
 ): Triangle[] {
   const endpoint_ = endpoint(p1, p2, length);
   const baseCaseTriangle = new Triangle(p1, p2, endpoint_);
@@ -79,32 +85,18 @@ function splitRecursively(
   ];
 }
 
-const edges = [
-  [seedTriangle.p1, seedTriangle.p3],
-  [seedTriangle.p3, seedTriangle.p2],
-  [seedTriangle.p2, seedTriangle.p1],
-];
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function draw() {
   background(0, 0, 16);
-  translate(width / 2, height / 2);
-
-  const recursions = ((value: string | number): number => {
-    if (typeof value === "string") {
-      console.log("got a string, and thats wrong");
-      return 5;
-    }
-    return value;
-  })(recursions_slider.value());
-
+  translate(widthBy2, heightBy2);
+  const recursions: number = parseInt(recursions_slider.value() as string);
   const mouse_len = (height - mouseY) / 8;
   const triangles = edges.flatMap(([p1, p2]: [Point, Point]) =>
-    splitRecursively(p1, p2, mouse_len, recursions),
+    splitRecursively(p1, p2, mouse_len, recursions)
   );
 
   const hueStep = 360 / triangles.length;
-  triangles.forEach((triangle_, index, _) => {
+  triangles.forEach((triangle_, index) => {
     const hue = (index * hueStep) / 2;
     fill(hue, 100, 100, 50);
     triangle(...triangle_.get());
