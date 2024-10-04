f2d07357194ff3ab7eace0584bfa7de808fa4d86
saygo-png <saygo.mail@proton.me>
2024-10-04 18:08:56 +0200

    better template

--- 2024/typescript/template/run.js
+++ 2024/typescript/template/run.js
@@ -1,6 +1,6 @@
 // helper script to run a cross-platform sketch with args
 const exec = require("child_process").exec;
-const arg = process.argv[2] || "helloworld"; // default to the hello world sketch
+const arg = process.argv[2] || "sketch"; // default to the sketch sketch
 
 const compileProcess = exec("npx tsc --watch --p ./sketches/"+arg) // stdin, out, err
 compileProcess.stdout.pipe(process.stdout);
